6666b49978f02e9ee72d07563a84673bf9bd50db
easy89757 <easy89757@126.com>
2019-12-13 17:37:49 +0800

    添加各个服务框架代码。

---
+++ src/main/java/com/cctv/zipkin/ZipKinApplication.java
@@ -0,0 +1,18 @@
+package com.cctv.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+/**
+ * @author zb
+ * @create 2019-12-13 10:04
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class,args);
+    }
+}
